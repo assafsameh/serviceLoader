0244179105104a6ad127c3ca63c993e3efedce2e
sam <assaf.sameh@gmail.com>
2025-06-14 17:05:51 +0400

    Add Remote Driver with firefox and chrome browsers option browser

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,11 +1,89 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
 public class SampleTest {
+
+    private RemoteWebDriver driver;
+
+    @BeforeTest
+    public void runBeforeTestMethod() {
+        try {
+            switch (System.getenv("test_browser").toLowerCase()) {
+                case "firefox":
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444"), new FirefoxOptions());
+                    break;
+                case "chrome":
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+                    break;
+                default:
+                    System.out.println("No browser defined!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @AfterTest
+    public void runAfterTestMethod(){
+        driver.quit();
+        System.out.println("*************************** Run After Test Method ***********************************************");
+    }
+
 
     @Test
     public void sampleTestMethod() {
         System.out.println("*************************** This is a sample test running***********************************************");
+
+        System.out.println("0. Start");
+
+        System.out.println("1. Open target page");
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().setSize(new Dimension(1350, 637));
+
+        System.out.println("2. Insert username and password");
+
+        System.out.println(" 2.1 Insert username");
+        driver.findElement(By.id("login_credentials")).click();
+        driver.findElement(By.cssSelector("*[data-test=\"username\"]")).click();
+        driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
+
+        System.out.println(" 2.1 Insert password");
+        driver.findElement(By.cssSelector(".login_password")).click();
+        driver.findElement(By.cssSelector("*[data-test=\"password\"]")).click();
+        driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+
+        System.out.println("3. Click submit to perform login");
+        driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+
+        System.out.println("4. Verify login has been successfully executed");
+        System.out.println(" 4.1 Page title is 'Swag Labs'");
+
+        /*Assert.assertEquals(driver.getTitle(), "Swag Labs");*/
+        Assert.assertEquals(driver.getTitle(), "Wrong Labs");
+
+        System.out.println(" 4.2 Page url contains 'inventory'");
+        Assert.assertEquals(driver.getCurrentUrl().contains("inventory"), true);
+
+        //Pause the execution for 2 seconds to show the logged in page
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("5. End");
     }
+
 }
