ee9c8514b9e67e6b7c4fb68cac947346d8f26558
sam <assaf.sameh@gmail.com>
2025-06-18 02:10:15 +0400

    Add Allure

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.qameta.allure.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -43,38 +44,47 @@ public class SampleTest {
         System.out.println("*************************** Run After Test Method ***********************************************");
     }
 
-
-    @Test
+    @Epic("First Epic")
+    @Feature("First Feature")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "Verify login with valid credentials", groups = "smoke")
     public void sampleTestMethod() {
         System.out.println("*************************** This is a sample test running***********************************************");
 
         System.out.println("0. Start");
 
         System.out.println("1. Open target page");
+
+        Allure.step("Navigaetion Step");
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().setSize(new Dimension(1350, 637));
 
         System.out.println("2. Insert username and password");
 
+        Allure.step("2.1 Insert username");
         System.out.println(" 2.1 Insert username");
         driver.findElement(By.id("login_credentials")).click();
         driver.findElement(By.cssSelector("*[data-test=\"username\"]")).click();
         driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
 
+        Allure.step("2.1 Insert password");
         System.out.println(" 2.1 Insert password");
         driver.findElement(By.cssSelector(".login_password")).click();
         driver.findElement(By.cssSelector("*[data-test=\"password\"]")).click();
         driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
 
+        Allure.step("3. Click submit to perform login");
         System.out.println("3. Click submit to perform login");
         driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
 
+        Allure.step("4. Verify login has been successfully executed");
         System.out.println("4. Verify login has been successfully executed");
         System.out.println(" 4.1 Page title is 'Swag Labs'");
 
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
         /*Assert.assertEquals(driver.getTitle(), "Wrong Labs");*/
 
+        Allure.step("4.2 Page url contains 'inventory'");
         System.out.println(" 4.2 Page url contains 'inventory'");
         Assert.assertEquals(driver.getCurrentUrl().contains("inventory"), true);
 
