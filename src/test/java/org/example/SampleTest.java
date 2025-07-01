22ddd23d54b8555cbd80a5233923ea4cd9856adb
sam <assaf.sameh@gmail.com>
2025-07-02 01:58:20 +0400

    bug fixes

--- src/test/java/org/example/SampleTest.java
+++ src/test/java/org/example/SampleTest.java
@@ -1,18 +1,9 @@
 package org.example;
 
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.net.URI;
-
 public class SampleTest {
-
-    @BeforeClass
-    public void BeforeClass() {
-        System.out.println("BeforeClass");
-    }
-
     @BeforeMethod
     public void setUp() {
         System.out.println("setUp");
